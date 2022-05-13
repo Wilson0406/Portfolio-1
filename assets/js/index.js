e9b75dba72d9383d41b4b335a27caf0801e64f05
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-05-13 21:20:42 +0530

    added dark mode

--- assets/js/index.js
+++ assets/js/index.js
@@ -53,4 +53,32 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.skills__data, .project__img, .contact__input',{interval: 200}); 
+
+
+// Dark Mode toggle
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
